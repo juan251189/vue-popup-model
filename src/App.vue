30602518c6fe689daed16a079234c141c8d63994
juan251189 <juan2511@hotmail.com>
2020-08-16 01:30:21 +1000

    Tutorial completed

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,11 @@ Show Modal
 
   </div>
 </transition>
+<transition name="slide"><div class="modal" v-if="showModal"><h1>Lorem</h1><p>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+<button class="button" @click="showModal = false">Close Modal</button>
+</div></transition>
   </div>
 </template>
 
@@ -76,4 +81,52 @@ display: flex;
   z-index: 98;
   background-color: rgba(0, 0, 0, 0.3);
 }
+.modal{
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 99;
+
+  width: 100%;
+  max-width: 400px;
+  background-color: #fff;
+  border-radius: 16px;
+
+
+padding: 25px;
+}
+
+.fade-enter-active,
+.fade-leave-active{
+  transition: opacity .5s;
+}
+.fade-enter-active,
+.fade-leave-to{
+  opacity: 0;
+}
+
+
+
+.slide-enter-active,
+.slide-leave-active{
+  transition: transform .5s;
+}
+.slide-enter,
+.slide-leave-to{
+  transform:translateY(-50%) translateX(100vw);
+}
+h1{
+  color: #222;
+  font-size: 32px;
+  font-weight: 900;
+  margin-bottom: 15px;
+}
+p{
+  color: #666;
+  font-size: 20px;
+  font-weight: 400;
+  margin-bottom: 15px;
+}
+
 </style>
