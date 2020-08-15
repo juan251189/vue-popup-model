d8bb7f0f8600b51ea5e115e64ddfbc91ebb03cbf
juan251189 <juan2511@hotmail.com>
2020-08-16 01:03:56 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+<button class="button" @click="showModal = true">
+Show Modal
+</button>
+<transition name="fade" appear>
+  <div class="modal-overlay" v-if="showModal"
+  @click="showModal = false">
+
+  </div>
+</transition>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      showModal:true
+    }
+  }
+
+
+}
+</script>
+
+<style>
+body{margin:0;
+padding:0;
+box-sizing: border-box;
+font-family: 'montserrat',sans-serif;
+}
+
+#app{
+display: flex;
+  position:relative;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  min-height: 100vh;
+  overflow-x: hidden;
+  border: 1px solid black;
+}
+.button{
+  appearance:none;
+  outline: none;
+  border:none;
+  background: none;
+  cursor:pointer;
+
+  display: inline-block;
+  padding: 15px 25px;
+  background-image: linear-gradient(to right,#CC2E5D,#FF5858);
+  border-radius: 8px;
+
+  color:#FFF;
+  font-size: 18px;
+  font-weight: 700;
+
+  box-shadow: 3px 3px rgba(0, 0, 0, 0.4);
+  transition: 0.4s ease-out;
+}
+.button:hover{
+  box-shadow: 6px 6px rgba(0, 0, 0, 0.6);
+}
+
+.modal-overlay{
+  position: absolute;
+  top:0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 98;
+  background-color: rgba(0, 0, 0, 0.3);
+}
+</style>
